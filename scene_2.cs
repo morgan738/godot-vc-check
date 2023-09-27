3cd45c46278d12249fc4bdfb3c2e47fa6316b5f4
morgan738 <37817281+morgan738@users.noreply.github.com>
2023-09-27 17:40:48 -0400

    test commit

---
+++ scene_2.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class scene_2 : Label
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	private void _on_button_pressed()
+	{
+		var global = GetNode<globalScript>("/root/GlobalScript");
+		global.GotoScene("res://scene_1.tscn");
+	}
+}
+
+
+
+
+
